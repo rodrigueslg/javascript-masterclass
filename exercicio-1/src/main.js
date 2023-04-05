30d2874e09be5796f8379bb82f74d7f744470200
Luis Rodrigues <luis.rodrigues@wunderkind.co>
2023-04-05 17:08:03 -0300

    regexp

---
+++ exercicio-1/src/main.js
@@ -0,0 +1,9 @@
+let statement = "create table author (id number, name string, age number, city string, state string, country string)";
+const regexp = /create table ([a-z]+) \((.+)\)/;
+const parsedStatement = statement.match(regexp);
+const tableName = parsedStatement[1]
+console.log(tableName)
+
+let columns = parsedStatement[2]
+columns = columns.split(", ")
+console.log(columns)
